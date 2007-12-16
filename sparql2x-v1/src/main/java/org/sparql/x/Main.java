67a2aa120904f08c74701ad5dd5ca6f44e8b683b
Juergen.Pfundt <Juergen.Pfundt@web.de>
2007-12-16 23:54:21 +0000

    Added Main.java for testing the grammars. Execute from sparql2x directory as follows: java -cp lib/runtime/antlr-runtime-3.0.1.jar;lib/runtime/stringtemplate-3.1b1.jar;lib/runtime/antlr2-2.7.7.jar;target/classes org.sparql.x.Main tmp.sql where tmp.sql contains a query.

---
+++ sparql2x-v1/src/main/java/org/sparql/x/Main.java
@@ -0,0 +1,22 @@
+package org.sparql.x;
+
+import org.antlr.runtime.*;
+import org.antlr.runtime.tree.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+
+		CharStream input = new ANTLRFileStream(args[0]);
+		SparqlLexer lexer = new SparqlLexer(input);
+		CommonTokenStream tokens = new CommonTokenStream(lexer);
+		SparqlParser parser = new SparqlParser(tokens);
+		SparqlParser.query_return t = parser.query();
+		System.out.println("tree="+((Tree)t.tree).toStringTree());
+		System.out.println("-----------------------------");
+		CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)t.tree);
+		nodes.setTokenStream(tokens);
+		SparqlT walker = new SparqlT(nodes);
+		walker.query();
+    System.out.flush();
+	}
+}
