b85ec6f50b074859fab8202fd047938fec90e0a9
Juergen.Pfundt <Juergen.Pfundt@web.de>
2007-12-21 00:21:05 +0000

    Added a few lines for testing template output.

--- sparql2x-v1/src/main/java/org/sparql/x/Main.java
+++ sparql2x-v1/src/main/java/org/sparql/x/Main.java
@@ -1,7 +1,13 @@
 package org.sparql.x;
 
+import java.io.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import org.antlr.runtime.*;
 import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
+import org.antlr.stringtemplate.language.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -12,11 +18,19 @@ public class Main {
 		SparqlParser parser = new SparqlParser(tokens);
 		SparqlParser.query_return t = parser.query();
 		System.out.println("tree="+((Tree)t.tree).toStringTree());
-		System.out.println("-----------------------------");
+		System.out.println("-------------------------------");
 		CommonTreeNodeStream nodes = new CommonTreeNodeStream((Tree)t.tree);
 		nodes.setTokenStream(tokens);
 		SparqlT walker = new SparqlT(nodes);
-		walker.query();
+		StringTemplateGroup templates = new StringTemplateGroup(new FileReader("json.stg"), DefaultTemplateLexer.class);
+    walker.setTemplateLib(templates);
+    /*
+    File out = new File("json.sql");
+    walker.setOutput(out);
+    */
+		RuleReturnScope r = walker.query();
+		System.out.println("+++++++++++++++++++++++++++++++");
+	  System.out.println(r.getTemplate().toString());
     System.out.flush();
 	}
 }
