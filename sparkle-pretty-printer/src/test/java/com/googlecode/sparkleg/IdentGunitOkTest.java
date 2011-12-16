0c06a80537959a9904bd04cdadf281d9e74825b3
juergen.pfundt <Juergen.Pfundt@gmail.com>
2011-12-16 22:58:22 +0000

    Added standard copyrigth note.

--- sparkle-pretty-printer/src/test/java/com/googlecode/sparkleg/IdentGunitOkTest.java
+++ sparkle-pretty-printer/src/test/java/com/googlecode/sparkleg/IdentGunitOkTest.java
@@ -1,8 +1,23 @@
+/*
+  Copyright 2007-2011 The sparkle-g Team
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
 package com.googlecode.sparkleg;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,6 +28,8 @@ import org.antlr.stringtemplate.*;
 import org.antlr.stringtemplate.language.*;
 
 import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import static org.junit.Assert.*;
@@ -21,8 +38,7 @@ import java.io.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+
 
 /**
  *
